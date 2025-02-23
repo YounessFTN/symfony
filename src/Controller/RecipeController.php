56ec098d9bc6023df47ed2cda9138e6bb8c36cf4
YounessFTN <youness.fatine1@gmail.com>
2025-02-23 17:39:42 +0100

    init

---
+++ src/Controller/RecipeController.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+
+final class RecipeController extends AbstractController
+{
+
+    #[Route('/manga', name: 'manga.index')]
+    public function index(Request $request): Response
+    {
+        return new JsonResponse([
+
+            'Chiara'=>"la connasse",
+        ]);
+
+    }
+
+
+    #[Route('/manga/{slug}-{id}', name: 'manga.show')]
+    public function show(Request $request, string $slug, int $id): Response
+    {
+        return new JsonResponse([
+            'slug' => $slug,
+            'id'=> $id,
+            'Chiara'=>"la connasse",
+        ]);
+
+        // return $this->render("recipe/show.html.twig",[
+        //     'slug' => $slug,
+        //     'id'=> $id,
+        //     'connasse'=>[
+        //         'name'=> 'chiara',
+        //         'age'=> 20,
+                
+        //     ]
+        // ]);
+    }
+}
